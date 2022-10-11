c377a1fca488144457c47f8f2fe7b1fd4467c71f
Ivan <infr4rot@gmail.com>
2022-10-11 20:16:21 +0700

    Небольшие правки

--- client/src/main/java/org/fnt/service/TimetableService.java
+++ client/src/main/java/org/fnt/service/TimetableService.java
@@ -15,60 +15,60 @@ public class TimetableService {
         this.messageService = messageService;
     }
 
-    public Message<Sendable> addTime(String employeeId, List<Timetable> timetableList) {
+    public Message<Sendable> addTime(String currentUser, List<Timetable> timetableList) {
         Message message = null;
         messageService.write(
                 new Message(
                         MessageType.REQUEST,
-                        employeeId,
+                        currentUser,
                         "-ADD_TIME",
                         timetableList));
         message = messageService.read();
         return message;
     }
 
-    public Message<Sendable> getFreeTime(String employeeId) {
+    public Message<Sendable> getFreeTime(String currentUser) {
         Message message = null;
         messageService.write(
                 new Message(
                         MessageType.REQUEST,
-                        employeeId,
+                        currentUser,
                         "-GET_FREE_TIME",
                         null));
         message = messageService.read();
         return message;
     }
 
-    public Message<Sendable> getAllTime(String employeeId) {
+    public Message<Sendable> getAllTime(String currentUser) {
         Message message = null;
         messageService.write(
                 new Message(
                         MessageType.REQUEST,
-                        employeeId,
+                        currentUser,
                         "-GET_ALL_TIME",
                         null));
         message = messageService.read();
         return message;
     }
 
-    public Message<Sendable> appoint(String userId, Timetable timetable) {
+    public Message<Sendable> appoint(String currentUser, Timetable timetable) {
         Message message = null;
         messageService.write(
                 new Message(
                         MessageType.REQUEST,
-                        userId,
+                        currentUser,
                         "-APPOINTMENT",
                         List.of(timetable)));
         message = messageService.read();
         return message;
     }
 
-    public Message<Sendable> getUserTimetable(String userId) {
+    public Message<Sendable> getUserTimetable(String currentUser) {
         Message message = null;
         messageService.write(
                 new Message(
                         MessageType.REQUEST,
-                        userId,
+                        currentUser,
                         "-GET_USER_TIMETABLE",
                         null));
         message = messageService.read();
